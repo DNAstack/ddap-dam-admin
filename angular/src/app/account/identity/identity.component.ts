b4f2a9fae7e0e6b4d603199e19b64393b973474c
Milan Panik <milan.panik1@gmail.com>
2020-07-21 11:53:39 +0200

    DISCO-2944 Display error in snackbar for personal info form

--- angular/src/app/account/identity/identity.component.ts
+++ angular/src/app/account/identity/identity.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import _get from 'lodash.get';
 import { Observable } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
 
@@ -12,9 +13,10 @@ import {
 } from '../../shared/users/user-account-close-confirmation-dialog/user-account-close-confirmation-dialog.component';
 import { UserService } from '../../shared/users/user.service';
 
-import IUser = scim.v2.IUser;
 import { IdentityService } from './identity.service';
 import { IdentityStore } from './identity.store';
+
+import IUser = scim.v2.IUser;
 
 @Component({
   templateUrl: './identity.component.html',
@@ -48,7 +50,13 @@ export class IdentityComponent implements OnInit {
   updatePersonalInfo(): void {
     const change = this.personalInfoForm.getModel();
     this.userService.patchLoggedInUser(change)
-      .subscribe(() => this.openSnackBar('Successfully update personal information. To take effect reload the page.'));
+      .subscribe(
+        () => this.openSnackBar('Successfully update personal information. To take effect reload the page.'),
+        ({ error }) => {
+          const defaultErrorMessage = 'Failed to update personal information. Please try again.';
+          this.openSnackBar(this.extractHumanReadableErrorMessage(error, defaultErrorMessage));
+        }
+      );
   }
 
   closeAccount(user: IUser) {
@@ -72,6 +80,13 @@ export class IdentityComponent implements OnInit {
     this.snackBar.open(message, null, {
       duration: 3000,
     });
+  }
+
+  private extractHumanReadableErrorMessage(error: object, defaultMessage?: string) {
+    const errorMessage: string = _get(error, 'message', defaultMessage);
+    return errorMessage.startsWith('operation 0:')
+      ? errorMessage.replace('operation 0:', '')
+      : errorMessage;
   }
 
 }
