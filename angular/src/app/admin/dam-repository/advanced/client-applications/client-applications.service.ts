9ae680add8ebf0e687fc9a1727714589dfc68c4a
Monica <monica@dnastack.com>
2020-07-29 11:23:54 -0400

    DISCO-3028 make client application config edit a PATCH request

--- angular/src/app/admin/dam-repository/advanced/client-applications/client-applications.service.ts
+++ angular/src/app/admin/dam-repository/advanced/client-applications/client-applications.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ErrorHandlerService } from 'ddap-common-lib';
+import { ConfigModificationModel, ErrorHandlerService, realmIdPlaceholder } from 'ddap-common-lib';
+import { Observable } from 'rxjs';
 
+import { environment } from '../../../../../environments/environment';
 import { DamConfigEntityType } from '../../shared/dam/dam-config-entity-type.enum';
 import { DamConfigService } from '../../shared/dam/dam-config.service';
 
@@ -17,4 +19,9 @@ export class ClientApplicationService extends DamConfigService {
     super(DamConfigEntityType.clients, http);
   }
 
+  update(entityId: string, change: ConfigModificationModel): Observable<any> {
+    return this.http.patch(`${environment.damApiUrl}/${realmIdPlaceholder}/config/${this.entityType}/${entityId}`,
+      change
+    );
+  }
 }
