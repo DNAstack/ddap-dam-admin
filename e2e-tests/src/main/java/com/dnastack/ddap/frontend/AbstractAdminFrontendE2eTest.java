7d74508aff17b47e2c29e868d140a5c1c231ef06
Milan Panik <milan.panik1@gmail.com>
2020-08-07 12:11:10 +0300

    Attempt to make test more reliable

--- e2e-tests/src/main/java/com/dnastack/ddap/frontend/AbstractAdminFrontendE2eTest.java
+++ e2e-tests/src/main/java/com/dnastack/ddap/frontend/AbstractAdminFrontendE2eTest.java
@@ -2,7 +2,11 @@ package com.dnastack.ddap.frontend;
 
 import com.dnastack.ddap.common.TestingPersona;
 import com.dnastack.ddap.common.page.AdminDdapPage;
+import org.junit.After;
 import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
@@ -28,6 +32,19 @@ public abstract class AbstractAdminFrontendE2eTest extends AbstractFrontendE2eTe
     protected static String getInternalName(String displayName) {
         String name = format("%s-%s", displayName, System.currentTimeMillis());
         return name.length() > 15 ? name.substring(0, 16) : name;
+    }
+
+    // Whenever test fails the JUnit retries the test, but it does not restart the test from the clean state.
+    // There might be unclosed dialogs or dropdowns which might prevent a valid test retry. We try to close those
+    // elements here if they exists after ending the test so the actual test retry won't be affected by them.
+    @After
+    public void tearDown() throws Exception {
+        try {
+            WebElement overlay = driver.findElement(By.className("cdk-overlay-backdrop"));
+            overlay.click();
+        } catch (NoSuchElementException nsee) {
+            // intentionally left empty
+        }
     }
 
 }
